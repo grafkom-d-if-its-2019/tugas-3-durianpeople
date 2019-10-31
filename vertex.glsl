72a76947360977bd919608d91ef681fe857b5275
Setya Wibawa <durianpeople@gmail.com>
2019-10-31 20:16:08 +0700

    final

--- vertex.glsl
+++ vertex.glsl
@@ -1,6 +1,13 @@
-attribute vec4 aPosition;
+precision mediump float;
+
+attribute vec3 vPosition;
+attribute vec3 vColor;
+varying vec3 fColor;
+uniform mat4 modelMatrix;
+uniform mat4 viewMatrix;
+uniform mat4 projectionMatrix;
 
 void main() {
-  gl_Position = aPosition;
-  gl_PointSize = 10.0;
+  fColor = vColor;
+  gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(vPosition, 1.0);
 }
